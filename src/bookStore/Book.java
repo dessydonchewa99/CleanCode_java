53cbb8c927d749ce3b9b5177738975b690140d63
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-05-26 15:59:21 +0300

    add check leap year add

--- src/bookStore/Book.java
+++ src/bookStore/Book.java
@@ -77,6 +77,34 @@ public class Book extends PrintEdition {
         }
         return false;
     }
+    public void checkLeapYear(){
+        boolean leap = false;
+        if(year % 4 == 0)
+        {
+            if(year % 100 == 0)
+            {
+                if (year % 400 == 0) {
+                    leap = true;
+                }
+                else {
+                    leap = false;
+                }
+            }
+            else {
+                leap = true;
+            }
+        }
+        else {
+            leap = false;
+        }
+
+        if(leap) {
+            System.out.println(year + " is a leap year.");
+        }
+        else{
+            System.out.println(year + " is not a leap year.");
+        }
+    }
 
     private class BookPrice implements PricePerPrintEdition {
 

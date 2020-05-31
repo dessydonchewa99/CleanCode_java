f4c1d4196df12e26236c755d84ae57e82c6fd539
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-05-31 23:17:42 +0300

    fix bugs

--- src/templates/PrintList.java
+++ src/templates/PrintList.java
@@ -1,19 +1,33 @@
 package templates;
 
+import bookStore.PricePerPrintEdition;
+import bookStore.PrintEdition;
 import bookStore.PrintEditionInformation;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
-public class PrintList<T extends PrintEditionInformation> {
+public class PrintList<T extends PrintEdition> {
     private List<T> objects;
 
+    public PrintList(){
+        objects = new ArrayList<>();
+    }
+
     public PrintList(List<T> objects) {
-        this.objects = objects;
+        this.objects = new ArrayList<>(objects.size());
+        for(T object: objects){
+            this.objects.add(object);
+        }
     }
 
     public void printObjects() {
         for(T object : objects){
-            object.printEditionInformation();
+            PrintEditionInformation printEditionInformation = object.getPrintInformation();
+            printEditionInformation.printEditionInformation();
+
+            System.out.println();
         }
     }
 }

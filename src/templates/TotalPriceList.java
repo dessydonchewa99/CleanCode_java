dee2ba7fd1c4ae38dfc4f43301962a967de2d06e
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-05-31 23:18:23 +0300

    remove price variable and make it local for total function

--- src/templates/TotalPriceList.java
+++ src/templates/TotalPriceList.java
@@ -1,22 +1,32 @@
 package templates;
 
 import bookStore.PricePerPrintEdition;
+import bookStore.PrintEdition;
+import bookStore.PrintEditionInformation;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class TotalPriceList<T extends PricePerPrintEdition>{
+public class TotalPriceList<T extends PrintEdition>{
     private List<T> objects;
-    private double price = 0;
+
+    public TotalPriceList(){
+        objects = new ArrayList<>();
+    }
 
     public TotalPriceList(List<T> objects) {
-        this.objects = objects;
+        this.objects = new ArrayList<>(objects.size());
+        for(T object: objects){
+            this.objects.add(object);
+        }
     }
 
     public double total() {
+        double price = 0.0;
         for(T object : objects){
-            price = price + object.calculatePricePerEdition();
+            PricePerPrintEdition pricePerPrintEdition = object.getPrintEditionPrice();
+            price = price + pricePerPrintEdition.calculatePricePerEdition();
         }
-
         return price;
     }
 }

44cb950dca8cff752a170b112e64b36c1462bd16
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-06-05 11:41:29 +0300

    add default constructor and update SortList(List<T> objects) constructor

--- src/templates/SortList.java
+++ src/templates/SortList.java
@@ -1,10 +1,21 @@
 package templates;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class SortList<T extends Comparable<T>> {
     List<T> objects;
+
+    public SortList(){
+        objects = new ArrayList<>();
+    }
+    public SortList(List<T> objects){
+        this.objects = new ArrayList<>(objects.size());
+        for(T object: objects){
+            this.objects.add(object);
+        }
+    }
 
     public List<T> sort() {
         Collections.sort(objects);

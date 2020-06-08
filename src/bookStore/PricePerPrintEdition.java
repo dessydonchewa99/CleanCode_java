cc2710951a4e1c9e3baedc7574496e31ba71ec73
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-06-08 16:43:40 +0300

    add javadoc for PricePerPrintEdition interface

--- src/bookStore/PricePerPrintEdition.java
+++ src/bookStore/PricePerPrintEdition.java
@@ -1,5 +1,14 @@
 package bookStore;
-
+/**
+ * Represents a PricePerPrintEdition interface.
+ * @author Desislava Dontcheva
+ * @version 1.0
+ */
 public interface PricePerPrintEdition {
+    /**
+     * This is an interface function that calculates
+     * price per edition.
+     * @return double This is the calculated price.
+     */
     double calculatePricePerEdition();
 }

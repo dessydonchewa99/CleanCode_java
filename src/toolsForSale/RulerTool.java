95d010c682cbf0044e9cc46be4afc4c9ea5507cb
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-06-10 16:35:54 +0300

    add javadoc and fix the constructor for RulerTool class

--- src/toolsForSale/RulerTool.java
+++ src/toolsForSale/RulerTool.java
@@ -1,13 +1,27 @@
 package toolsForSale;
-
+/**
+ * Represents a RulerTool.
+ * @author Desislava Dontcheva
+ * @version 1.0
+ */
 import exceptions.ToolMeasureException;
 
 public class RulerTool extends GeometryTool {
 
-    public RulerTool(GeometryToolMeasure centimeter){
+    /**
+     * Constructor with parameters for class RulerTool.
+     * @param measure This is the measure of RulerTool.
+     */
+    public RulerTool(GeometryToolMeasure measure){
         super();
+        validationMeasure(new RulerTool(measure));
     }
 
+    /**
+     * This method validates if the measure of RulerTool
+     * is correct or not.
+     * @param ruler Object of class RulerTool.
+     */
     public void validationMeasure(RulerTool ruler){
         ToolMeasureException exception = new ToolMeasureException();
         exception.rulerValidation(ruler, "Invalid ruler measure");

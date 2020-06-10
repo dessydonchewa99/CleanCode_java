b4d148685bf003a932defcf894224e9f2e1f52ef
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-06-10 16:31:07 +0300

    add javadoc for PrintList class

--- src/templates/PrintList.java
+++ src/templates/PrintList.java
@@ -1,25 +1,36 @@
 package templates;
-
-import bookStore.PricePerPrintEdition;
+/**
+ * Represents a PrintList.
+ * @author Desislava Dontcheva
+ * @version 1.0
+ */
 import bookStore.PrintEdition;
 import bookStore.PrintEditionInformation;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PrintList<T extends PrintEdition> {
     private List<T> objects;
 
+    /**
+     * Default constructor for class PrintList.
+     */
     public PrintList(){
         objects = new ArrayList<>();
     }
-
+    /**
+     * Constructor with parameters for class PrintList.
+     * @param objects This is an input Template List.
+     */
     public PrintList(List<T> objects) {
         this.objects = new ArrayList<>(objects.size());
         this.objects.addAll(objects);
     }
 
+    /**
+     * This method prints all Template type objects.
+     */
     public void printObjects() {
         for(T object : objects){
             PrintEditionInformation printEditionInformation = object.getPrintInformation();

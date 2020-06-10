2b943cd8eac6dfbd918a9a906abf464e56887c8c
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-06-10 16:34:13 +0300

    add javadoc for PenColors enum type

--- src/toolsForSale/PenColors.java
+++ src/toolsForSale/PenColors.java
@@ -1,7 +1,14 @@
 package toolsForSale;
-
+/**
+ * Represents a PenColors enum type.
+ * @author Desislava Dontcheva
+ * @version 1.0
+ */
 enum PenColors{
 
+    /**
+     * Possible Pen colors.
+     */
     BLACK("BLACK"),
     RED("RED"),
     BLUE("BLUE"),
@@ -9,9 +16,18 @@ enum PenColors{
 
     private final String color;
 
+    /**
+     * Constructor with parameters for enum PenColors.
+     * @param color
+     */
     PenColors(String color){
         this.color = color == null ? "BLACK" : color;
     }
+
+    /**
+     * This method returns formatted string.
+     * @return String
+     */
     @Override
     public String toString() {
         return String.format("Pen color: %s%n", color);

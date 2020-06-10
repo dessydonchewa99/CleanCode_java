dc5e7d75f745e9c108c96e8e2a3a4d93a02501e3
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-06-10 16:36:09 +0300

    add javadoc and fix the constructor for TriangleTool class

--- src/toolsForSale/TriangleTool.java
+++ src/toolsForSale/TriangleTool.java
@@ -1,13 +1,27 @@
 package toolsForSale;
-
+/**
+ * Represents a TriangleTool.
+ * @author Desislava Dontcheva
+ * @version 1.0
+ */
 import exceptions.ToolMeasureException;
 
 public class TriangleTool extends GeometryTool {
 
-    public TriangleTool(GeometryToolMeasure centimeter) {
+    /**
+     * Constructor with parameters for class TriangleTool.
+     * @param measure This is the measure of TriangleTool.
+     */
+    public TriangleTool(GeometryToolMeasure measure) {
         super();
+        validationMeasure(new TriangleTool(measure));
     }
 
+    /**
+     * This method validates if the measure of TriangleTool
+     * is correct or not.
+     * @param triangle Object of class TriangleTool.
+     */
     public void validationMeasure(TriangleTool triangle) {
         ToolMeasureException exception = new ToolMeasureException();
         exception.triangleValidation(triangle, "Invalid triangle measure");

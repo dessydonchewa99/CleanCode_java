e2e242078216b1d234a7ae87f01ac01d8cbd5c33
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-06-10 16:34:32 +0300

    add javadoc for PenTypes enum type

--- src/toolsForSale/PenTypes.java
+++ src/toolsForSale/PenTypes.java
@@ -1,6 +1,14 @@
 package toolsForSale;
-
+/**
+ * Represents a PenTypes enum type.
+ * @author Desislava Dontcheva
+ * @version 1.0
+ */
 public enum PenTypes {
+
+    /**
+     * Possible Pen types.
+     */
     VERY_THIN("VERY_THIN"),
     THIN("THIN"),
     MEDIUM("MEDIUM"),
@@ -9,9 +17,18 @@ public enum PenTypes {
 
     private final String type;
 
+    /**
+     * Constructor with parameters for enum PenTypes.
+     * @param type
+     */
     PenTypes(String type){
         this.type = type == null ? "MEDIUM" : type;
     }
+
+    /**
+     * This method returns formatted string.
+     * @return String
+     */
     @Override
     public String toString() {
         return String.format("Pen type: %s%n", type);

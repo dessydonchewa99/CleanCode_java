e5dea625f20691108bf40393253361144cb6b91d
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-06-10 16:31:18 +0300

    add javadoc for SortList class

--- src/templates/SortList.java
+++ src/templates/SortList.java
@@ -1,5 +1,9 @@
 package templates;
-
+/**
+ * Represents a SortList.
+ * @author Desislava Dontcheva
+ * @version 1.0
+ */
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -7,14 +11,25 @@ import java.util.List;
 public class SortList<T extends Comparable<T>> {
     List<T> objects;
 
+    /**
+     * Default constructor for class SortList.
+     */
     public SortList(){
         objects = new ArrayList<>();
     }
+    /**
+     * Constructor with parameters for class SortList.
+     * @param objects This is an input Template List.
+     */
     public SortList(List<T> objects){
         this.objects = new ArrayList<>(objects.size());
         this.objects.addAll(objects);
     }
 
+    /**
+     * This method sorts all Template type objects.
+     * @return objects This is the sorted list.
+     */
     public List<T> sort() {
         Collections.sort(objects);
         return objects;

a8da5a1ba5f8fa7087e42d9e37616ae93bcc6184
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-06-10 16:31:48 +0300

    add javadoc for TotalPriceList class

--- src/templates/TotalPriceList.java
+++ src/templates/TotalPriceList.java
@@ -1,5 +1,9 @@
 package templates;
-
+/**
+ * Represents a TotalPriceList.
+ * @author Desislava Dontcheva
+ * @version 1.0
+ */
 import bookStore.PricePerPrintEdition;
 import bookStore.PrintEdition;
 import bookStore.PrintEditionInformation;
@@ -10,15 +14,25 @@ import java.util.List;
 public class TotalPriceList<T extends PrintEdition>{
     private List<T> objects;
 
+    /**
+     * Default constructor for class TotalPriceList.
+     */
     public TotalPriceList(){
         objects = new ArrayList<>();
     }
-
+    /**
+     * Constructor with parameters for class TotalPriceList.
+     * @param objects This is an input Template List.
+     */
     public TotalPriceList(List<T> objects) {
         this.objects = new ArrayList<>(objects.size());
         this.objects.addAll(objects);
     }
 
+    /**
+     * This method calculates the price of all Template type objects.
+     * @return double This is the total price.
+     */
     public double total() {
         double price = 0.0;
         for(T object : objects){

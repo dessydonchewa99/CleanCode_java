29bbf7fb13fadb61a64be59b185edc558f182a63
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-06-10 16:36:29 +0300

    add javadoc and fix the constructor for ProtractorTool class

--- src/toolsForSale/ProtractorTool.java
+++ src/toolsForSale/ProtractorTool.java
@@ -1,13 +1,27 @@
 package toolsForSale;
-
+/**
+ * Represents a ProtractorTool.
+ * @author Desislava Dontcheva
+ * @version 1.0
+ */
 import exceptions.ToolMeasureException;
 
 public class ProtractorTool extends GeometryTool {
 
-    public ProtractorTool(GeometryToolMeasure centimeter){
+    /**
+     * Constructor with parameters for class ProtractorTool.
+     * @param measure This is the measure of ProtractorTool.
+     */
+    public ProtractorTool(GeometryToolMeasure measure){
         super();
+        validationMeasure(new ProtractorTool(measure));
     }
 
+    /**
+     * This method validates if the measure of ProtractorTool
+     * is correct or not.
+     * @param protractor Object of class ProtractorTool.
+     */
     public void validationMeasure(ProtractorTool protractor){
         ToolMeasureException exception = new ToolMeasureException();
         exception.protractorValidation(protractor, "Invalid protractor measure");

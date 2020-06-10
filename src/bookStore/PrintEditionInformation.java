81ce7e0e7c59355ffbbded231eb6104e07f039ab
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-06-10 16:29:23 +0300

    add javadoc for PrintEditionInformation interface

--- src/bookStore/PrintEditionInformation.java
+++ src/bookStore/PrintEditionInformation.java
@@ -1,5 +1,13 @@
 package bookStore;
-
+/**
+ * Represents a PrintEditionInformation interface.
+ * @author Desislava Dontcheva
+ * @version 1.0
+ */
 public interface PrintEditionInformation {
+    /**
+     * This is an interface function that shows
+     * information about print edition.
+     */
     void printEditionInformation();
 }
